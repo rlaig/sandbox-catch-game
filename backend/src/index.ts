3eecd91de50a2833701ea1b90cef29d22fa8e9e6
raymond.laig <raymond.laig@lalamove.com>
2023-01-11 18:31:15 +0800

    feat: add deps

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,13 @@
 import { initDatabase } from './database';
 import { initApp } from './app';
 import { config } from './config'
+import { Server } from 'socket.io'
 
 const main = async () => {
   try {
     const { database, connection } = await initDatabase();
     const app = await initApp(database, connection);
+    const io = new Server(3000)
 
     app.listen(config.port, async () => {
       console.log(`Server is running at http://localhost:${config.port}`)
